d6943c34a60de20df933514c4878a80ab229643a
Temnij <kzorin52@gmail.com>
2021-09-15 19:19:31 +0600

    start

---
+++ DeBankAPI.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DeBank;
+public class DeBankAPI
+{
+    private HttpClient client;
+    public DeBankAPI()
+    {
+        client = new HttpClient();
+    }
+    public async Task<List<Models.Datum>> ScanAddress(string address)
+    {
+        var wReq = await client.GetAsync($"https://api.debank.com/token/balance_list?is_all=true&user_addr={address}");
+        return JsonConvert.DeserializeObject<Models.Root>(await wReq.Content.ReadAsStringAsync())?.Data ?? new();
+    }
+    public double GetDouble(object? _int, int decimals) => double.Parse((_int ?? 0d).ToString() ?? "0") / Math.Pow(10, decimals);
+    public double Round(double? toRound) => Math.Round(toRound ?? 0d, 2);
+}
---
+++ Models.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace DeBank.Models;
+public class Datum
+{
+    [JsonProperty("balance")]
+    public object? Balance;
+
+    [JsonProperty("chain")]
+    public string? Chain;
+
+    [JsonProperty("decimals")]
+    public int Decimals;
+
+    [JsonProperty("display_symbol")]
+    public object? DisplaySymbol;
+
+    [JsonProperty("id")]
+    public string? Id;
+
+    [JsonProperty("is_core")]
+    public bool? IsCore;
+
+    [JsonProperty("is_verified")]
+    public bool? IsVerified;
+
+    [JsonProperty("is_wallet")]
+    public bool IsWallet;
+
+    [JsonProperty("logo_url")]
+    public string? LogoUrl;
+
+    [JsonProperty("name")]
+    public string? Name;
+
+    [JsonProperty("optimized_symbol")]
+    public string? OptimizedSymbol;
+
+    [JsonProperty("price")]
+    public double? Price;
+
+    [JsonProperty("protocol_id")]
+    public string? ProtocolId;
+
+    [JsonProperty("symbol")]
+    public string? Symbol;
+
+    [JsonProperty("time_at", NullValueHandling = NullValueHandling.Ignore)]
+    public double TimeAt;
+}
+
+public class Root
+{
+    [JsonProperty("_cache_seconds")]
+    public int CacheSeconds;
+
+    [JsonProperty("_seconds")]
+    public double Seconds;
+
+    [JsonProperty("data")]
+    public List<Datum>? Data;
+
+    [JsonProperty("error_code")]
+    public int ErrorCode;
+}
